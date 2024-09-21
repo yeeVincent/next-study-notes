75bbbb77d65d9208d430169d1bdfd6b96f158744
曹谊的home <cao1032398875@sina.com>
2024-09-21 18:51:29 +0800

    add: 练习page层级关系

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,4 +1,6 @@
+
 import type { Metadata } from "next";
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        这是根layout
         {children}
       </body>
     </html>
---
+++ my-app/app/template.tsx
@@ -0,0 +1,4 @@
+
+export default function Template ({ children } : { children: React.ReactNode }) {
+  return <div>这是template { children }</div>
+}
